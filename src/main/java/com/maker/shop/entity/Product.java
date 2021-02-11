33f7f2294a969e08a47fffbaca0cfd8941030fea
Sunky97 <hsk11261612@gmail.com>
2021-02-11 16:33:17 +0900

    Add to BaseEntity

--- src/main/java/com/maker/shop/entity/Product.java
+++ src/main/java/com/maker/shop/entity/Product.java
@@ -1,4 +1,27 @@
 package com.maker.shop.entity;
 
-public class Product {
+import lombok.ToString;
+import javax.persistence.*;
+
+@Entity
+@Table(name= "product")
+@ToString
+public class Product extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long product_id;
+
+    private String product_name;
+
+    private Long product_price;
+
+    private String product_category;
+
+    private String product_brand;
+
+    private Long product_sale;
+
+    private String product_gender;
+
 }
