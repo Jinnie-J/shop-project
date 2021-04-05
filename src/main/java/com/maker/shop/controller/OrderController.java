c6c9a82b08371e100d39e9eab5f23083a2808a0b
Sunky97 <hsk11261612@gmail.com>
2021-04-05 18:46:43 +0900

    구매상품정보(수량,사이즈) 구매페이지로 넘기기 가능

--- src/main/java/com/maker/shop/controller/OrderController.java
+++ src/main/java/com/maker/shop/controller/OrderController.java
@@ -1,5 +1,8 @@
 package com.maker.shop.controller;
 
+import com.maker.shop.dto.ProductDTO;
+import com.maker.shop.dto.ProductSizeDTO;
+import com.maker.shop.entity.Product;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,11 +26,25 @@ public class OrderController {
 
     }
     @PostMapping("/order")
-    public void order(Model model, @RequestParam("cntArr") String[] cntArr, @RequestParam("sizeArr") String[] sizeArr, String imgUrl){
+    public void order(Model model, @RequestParam("cntArr") String[] cntArr, @RequestParam("sizeArr") String[] sizeArr, String imgUrl,Long price,String name, String brand){
 
-        model.addAttribute("cntArr",cntArr);
-        model.addAttribute("sizeArr",sizeArr);
+        List<ProductSizeDTO> sizeDTO = new ArrayList<ProductSizeDTO>();
+
+        for(int i=0; i<sizeArr.length; i++){
+
+            sizeDTO.add(new ProductSizeDTO(Long.parseLong(cntArr[i]),Long.parseLong(sizeArr[i])));
+        }
+
+        ProductDTO product = new ProductDTO();
+        product.setName(name);
+        product.setPrice(price);
+        product.setBrand(brand);
+
+        log.info("product : "+ product);
+
+        model.addAttribute("sizeDTO",sizeDTO);
         model.addAttribute("imgUrl",imgUrl);
+        model.addAttribute("product",product);
 
     }
 }
